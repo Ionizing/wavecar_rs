07efeaa6b071fba554b66fafeb7717fd52c71477
Ionizing <PeterSmith_9@outlook.com>
2020-08-20 17:18:28 +0800

    [tests/h2o_gam_5_2] update vasp5.2 gam test file

--- tests/h2o_gam_5_2/h2o_gam_5_2.rs
+++ tests/h2o_gam_5_2/h2o_gam_5_2.rs
@@ -10,6 +10,8 @@ mod test {
     use wavecar_rs::wavecar::{WavecarType, GammaHalfDirection};
     use vasp_poscar::Poscar;
     use vaspchg_rs::ChgType;
+    use ndarray::s;
+    use num::complex::Complex64 as c64;
     // use core::panicking::panic_fmt;
 
     #[test]
@@ -17,8 +19,17 @@ mod test {
         let mut wavecar = Wavecar::from_file(&get_fpath_in_current_dir!("WAVECAR"))?;
         wavecar.set_wavecar_type(WavecarType::GammaHalf(GammaHalfDirection::Z));
 
-        let fft_grid = wavecar.generate_fft_grid(0);
+        let coeffs = wavecar.read_wavefunction_coeffs(0, 0, 0).unwrap();
+        assert_eq!(coeffs.len(), 4658);
+        assert_eq!(coeffs[0], c64::new(-0.14767155051231384, 0.0));
+        assert_eq!(coeffs[4657], c64::new(-0.0003599866759032011, -0.000024752824174356647));
 
+        let fft_grid = wavecar.generate_fft_grid(0);
+        assert_eq!(fft_grid.len(), 4658);
+        assert_eq!(fft_grid[4657], &[0, -1, 13]);
+        assert_eq!(fft_grid[2000], &[9, 7, 4]);
+
+        let wfcr = wavecar.get_wavefunction_in_realspace_default_ngrid(0, 0, 0).unwrap();
         Ok(())
     }
 
@@ -26,7 +37,7 @@ mod test {
     fn test_wavecar_in_realspace() -> io::Result<()> {
         let path = get_fpath_in_current_dir!("WAVECAR");
         let mut wavecar = Wavecar::from_file(&path)?;
-        wavecar.set_wavecar_type(WavecarType::GammaHalf(GammaHalfDirection::Z));
+        wavecar.set_wavecar_type(WavecarType::GammaHalf(GammaHalfDirection::X));
 
         let poscar = Poscar::from_path(&get_fpath_in_current_dir!("POSCAR")).unwrap();
         wavecar.get_wavefunction_in_realspace_default_ngrid(0, 0, 5)
