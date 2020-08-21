a03574d3c6253af9fee218f41ccf068c64df4b8e
Ionizing <PeterSmith_9@outlook.com>
2020-08-21 17:05:15 +0800

    [cleanup.sh] update

--- tests/cleanup.sh
+++ tests/cleanup.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env sh
 
-rm -f CHG* \
-CONTCAR \
-DOSCAR \
-EIGENVAL \
-IBZKPT \
-OSZICAR \
-PCDAT \
-POTCAR \
-PROCAR \
-REPORT \
-XDATCAR \
-out* \
-sub_vasp* \
-vasprun.xml \
-vdw* \
-INCAR \
-KPOINTS
+
+curr_dir=$(pwd)
+for dir in $(ls -d */); do
+  cd $curr_dir/$dir
+  rm -vf CHG* \
+    CONTCAR \
+    DOSCAR \
+    EIGENVAL \
+    IBZKPT \
+    OSZICAR \
+    PCDAT \
+    POTCAR \
+    PROCAR \
+    REPORT \
+    XDATCAR \
+    out* \
+    sub_vasp* \
+    vasprun.xml \
+    vdw* \
+    INCAR \
+    KPOINTS \
+    *.vasp
+done
