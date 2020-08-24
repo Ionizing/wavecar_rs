9e4e60f1a44121c156a462b26b77fdf06a903f92
Ionizing <PeterSmith_9@outlook.com>
2020-08-24 17:18:14 +0800

    [doc] continues

--- src/lib.rs
+++ src/lib.rs
@@ -72,7 +72,7 @@
 //! band coefficients are stored.
 //!
 //! Here is the structure, from the third record:
-//! ```no_run
+//! ```text
 //! for ispin in 0..NSPIN {
 //!     for ikpoint in 0..NKPTS {
 //!         // One record here          1)
@@ -103,10 +103,91 @@
 //!     coeff[0], coeff[1], coeff[2], ...       // NPLWS values in total
 //!     ```
 //!
+//! To sum up, the number of total records in WAVECAR should be `2 + NSPIN * NKPTS * (1 + NBANDS)`.
 //!
-//! This crate only works on little endian machine. You can patch it if you have some feature
+//! However, for the calculations that enables spin orbits coupling correction, `NSPIN = 1`, but on
+//! each band, two spinor components (equal length) are stored, where the upper and the lower is
+//! are placed in order.
+//! ```text
+//! // on each band, spinor up
+//! coeffs[0], coeffs[1], ...
+//! //               spinor down
+//! coeffs[0], coeffs[1], ...
+//! ```
+//!
+//! **This crate only works on little endian machine. You can patch it if you have some feature
 //! requests on big endian machine, that's not difficult (changing all the LittleEndian generic
-//! parameter to BigEndian shall do the job).
+//! parameter to BigEndian shall do the job).**
+//!
+//!
+//! # Implementation of transformation of wavefunction in k-space into real-space
+//! We've got the band coefficients, but how to use it? What should we do if we want to visualize it in
+//! real space?  The relation between k-space and real space is the Fourier transformation. But in which
+//! order is the coefficients placed?
+//!
+//! ## FFT grid generation for standard & SOC system
+//! Wavefunction in k-space is a 3D grid, but we must start with 1D to illustrate how the grid is
+//! generated:
+//!
+//! Suppose we have a 1D grid whose size is _ngrid_, the 1D fft grid should be
+//! ```text
+//! [0, 1, 2, ... ngrid/2] ++ [(1+ngrid/2-ngrid), (ngrid/2-ngrid), ... -1]
+//! e.g. when ngrid = 11
+//! ret = [0, 1, 2, 3, 4, 5, -5, -4, -3, -2, -1]
+//! ```
+//!
+//! And for 3D  grid, three directions does the same job
+//! ```text
+//! fx = generate_grid(ngrid[0])
+//! fy = generate_grid(ngrid[1])
+//! fz = generate_grid(ngrid[2])
+//! ```
+//!
+//! Then combine them (in Fortran, the inner index is the fastest one)
+//! ```text
+//! for ifz in fz {
+//! for ify in fy {
+//! for ifx in fx {
+//!     fft_grid += [ifx, ify, ifz]
+//! }}}
+//! ```
+//! Each coordinate `[ifx, ify, ifz]` in the above will be named with `G` in the following.
+//!
+//! Now we've a cube in k-space. However the valid wavefunction grid should be a sphere where the
+//! radius is determined by the formula `(G + k)^2 / 2 < ENCUT`, where `k` is the k-vector of current
+//! k-point.
+//!
+//! We've rubbed the k-space cube into a k-space sphere, then the coefficients from WAVECAR can be
+//! placed into the grid in correspondence.
+//!
+//! Then the arrangement is done for standard and SOC systems. As for the gamma only system, the
+//! arrangement is somewhat more complicate.
+//!
+//! ## FFT grid generation for gamma only system
+//!
+//! First we need to perform a standard FFT grid generation, then filter the G points, i.e. cut the
+//! sphere and remove half of it. For gamma half of `x` direction:
+//! ```text
+//! fft_grid.iter()
+//!     .filter(|[gx, gy, gz]| {
+//!     (gx > 0) ||
+//!     ((gx == 0) && (gy > 0)) ||
+//!     ((gx == 0) && (gy == 0) && (gz >= 0)
+//! })
+//! ```
+//! And for 'z' direction:
+//! ```text
+//! fft_grid.iter()
+//!     .filter(|[gx, gy, gz]| {
+//!     (gz > 0) ||
+//!     ((gz == 0) && (gy > 0)) ||
+//!     ((gz == 0) && (gy == 0) && (gx >= 0))
+//! })
+//! ```
+//!
+//! ## Reverse Fourier transformation
+//!
+//!
 
 pub use error::WavecarError;
 pub use wavecar::Wavecar;
