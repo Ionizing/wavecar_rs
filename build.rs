1d400d8e2efcf317c764f4893026875e8bbc4a7c
Ionizing <PeterSmith_9@outlook.com>
2020-09-08 17:03:02 +0800

    [Cargo.toml] update, use system openblas and fftw

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 #[cfg(target_os = "macos")]
 fn main() {
-    println!("cargo:rustc-link-search=/usr/local/opt/gcc/lib/gcc/10")
+    println!("cargo:rustc-link-search=/usr/local/opt/gcc/lib/gcc/10");
+    println!("cargo:rustc-link-search=/usr/local/opt/openblas/lib");
 }
 
 #[cfg(not(target_os = "macos"))]
